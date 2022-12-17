45b4a50f8378bc5e843c5472198cf1c4f6a88d88
Shashank Tripathi <shashanktripathi.st15a@gmail.com>
2022-12-18 00:52:29 +0530

    update note is done

--- routes/notes.js
+++ routes/notes.js
@@ -2,19 +2,70 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const Notes = require('../models/Notes');
+const GetUser = require('../middleware/GetUser');
+const { findOne } = require('../models/Notes');
 
-router.post('/create-note',[
+//create-note : post
+router.post('/create-note',GetUser, [
     body('title', "Title field cant be empty").exists(),
     body('description', "Description field cant be empty").exists()
 ], async (req, res) => {
 
-    let note = await Notes.create({
-        title: req.body.title,
-        description: req.body.description,
-        tag: req.body.tag
-    })
-    console.log(note);
-    res.json(note);
-})
+    let valerrors = validationResult(req);
+    if (!valerrors.isEmpty()){
+        return res.status(400).json({ errors: valerrors.array()});
+    }
 
+    let { title, description, tag } = req.body;
+
+    try {
+        let note = Notes({
+            user: req.user.id,
+            title: title,
+            description: description,
+            tag: tag
+        })
+
+        let SvdNote = await note.save();
+        res.json(SvdNote);
+    } catch (error) {
+        res.status(401).send({error: error.message})
+    }
+});
+
+
+// get-all-notes : get
+router.get('/get-allnote', GetUser, async (req, res) => {
+
+    const notes = await Notes.find({user: req.user.id});
+
+    res.json(notes);
+
+});
+
+// update-note/:id : put
+
+router.put('/update-note/:id', GetUser, async (req, res) => {
+
+    const {title, description, tag} = req.body;
+    
+    let newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    let note = await Notes.findById(req.params.id);
+    if(!note){
+        return res.status(404).send({error: "Not Found"});
+    }
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send({error: "Bad Request"});
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+
+    res.send(note);
+
+})
 module.exports = router;
